6b52cb0b362d189cb09e55a7d55d2dc0c6901a23
steverhee3 <75031054+steverhee3@users.noreply.github.com>
2021-01-17 11:13:47 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,49 @@
+
+
+/*newstuffhere*/
+const container = document.querySelector('.container');
+
+const unsplashURL = 'https://source.unsplash.com/random/';
+
+const rows = 500
+
+for(let i=0; i < rows* 3; i++) {
+  const div= document.createElement('div');
+  div.classList.add('box');
+  
+  const  p =document.createElement('p');
+  const img = document.createElement('img');
+  img.src = `${unsplashURL}${getRandomSize()}`;
+  p.appendChild(img);
+  div.appendChild(p);
+  container.appendChild(div);
+}
+
+function getRandomSize() {
+  return `${getRandomNr()}x${getRandomNr()}`
+}
+function getRandomNr() {
+  return Math.floor(Math.random() * 1000) + 300
+}
+
+
+/*old stuff here */
+const boxes = document.querySelectorAll('.box')
+window.addEventListener('scroll', checkBoxes)
+checkBoxes()
+function checkBoxes() {
+  const triggerBottom = (window.innerHeight /5 *4)
+  
+  boxes.forEach((box) => {
+    const boxTop = box.getBoundingClientRect().top
+    
+    if(boxTop < triggerBottom) {
+      box.classList.add('show')
+      
+    }
+    else {
+      box.classList.remove('show')
+    }
+  })
+  
+}
